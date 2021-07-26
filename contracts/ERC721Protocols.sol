37ae23fdd9ffb5ee44532eeb5a4668fa2d9533dd
Artem B <artem.bv@protonmail.com>
2021-07-26 09:09:11 +0300

    update contract

--- contracts/ERC721Protocols.sol
+++ contracts/ERC721Protocols.sol
@@ -7,6 +7,10 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 interface IERC721ERC20Protocols {
 
+  event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
+  event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId);
+  event ApprovalForAll(address indexed owner, address indexed operator, bool approved);
+
   function getMyBalance() external view returns (uint256 balance);
   function getMyBalance(uint256 protocolId) external view returns (uint256 balance);
 
@@ -15,42 +19,40 @@ interface IERC721ERC20Protocols {
 
   function ownerOf(uint256 protocolId, uint256 tokenId) external view returns (address owner);
 
-  // function transferMy(address to, uint256 tokenId) external returns (bool);
+  function transferMy(address to, uint256 tokenId) external returns (bool);
   function transferMy(uint256 protocolId, address to, uint256 tokenId) external returns (bool);
 
-  // function safeTransferFrom(
-  //       uint256 protocolId,
-  //       address from,
-  //       address to,
-  //       uint256 tokenId,
-  //       uint256 value
-  //   ) external;
+  function safeTransferFrom(
+        uint256 protocolId,
+        address from,
+        address to,
+        uint256 tokenId
+    ) external;
   
-  // function safeTransferFrom(
-  //       uint256 protocolId,
-  //       address from,
-  //       address to,
-  //       uint256 tokenId,
-  //       uint256 value,
-  //       bytes calldata data
-  //   ) external;
+  function safeTransferFrom(
+        uint256 protocolId,
+        address from,
+        address to,
+        uint256 tokenId,
+        bytes calldata data
+    ) external;
 
-  // function transferFrom(
-  //       uint256 protocolId,
-  //       address from,
-  //       address to,
-  //       uint256 value,
-  //       uint256 tokenId
-  //   ) external;
+  function transferFrom(
+        uint256 protocolId,
+        address from,
+        address to,
+        uint256 tokenId
+    ) external;
 
   function approve(uint256 protocolId, address to, uint256 tokenId) external;
 
   function getApproved(uint256 protocolId, uint256 tokenId) external view returns (address operator);
 
-  // function setApprovalForAll(address operator, bool _approved) external;
-  function setApprovalForAll(uint256 protocolId, address operator, bool _approved) external;
+  function setApprovalForAll(address operator, bool _approved) external;
+  // ?
+  // function setApprovalForAll(uint256 protocolId, address operator, bool _approved) external;
 
-  // function isApprovedForAll(uint256 protocolId, address owner, address operator) external view returns (bool);
+  function isApprovedForAll(address owner, address operator) external view returns (bool);
 }
 
 contract ERC721ERC20Protocols is IERC721ERC20Protocols {
@@ -58,6 +60,7 @@ contract ERC721ERC20Protocols is IERC721ERC20Protocols {
   mapping(address => uint256)[] private balances;
   mapping(uint256 => address)[] private owners;
   mapping(uint256 => address)[] private approved;
+  mapping(address => mapping(address => bool)) private operatorsApproved;
 
   modifier protocolExists(uint256 protocolId) {
     require(protocolId < balances.length, "protocolExists: The protocold with such ID doesn't exist");
@@ -97,8 +100,18 @@ contract ERC721ERC20Protocols is IERC721ERC20Protocols {
     return owners[protocolId][tokenId];
   }
 
-  // function transferMy(address to, uint256 tokenId) external override returns (bool) {
-  // }
+  function transferMy(address to, uint256 tokenId)
+  external
+  override
+  validAddress(to)
+  returns (bool) {
+    for (uint i = 0; i < owners.length; i++) {
+      if (!_transfer(i, msg.sender, to, tokenId)) {
+        return false;
+      }
+    }
+    return true;
+  }
 
   function transferMy(uint256 protocolId, address to, uint256 tokenId)
   external
@@ -106,13 +119,67 @@ contract ERC721ERC20Protocols is IERC721ERC20Protocols {
   protocolExists(protocolId)
   validAddress(to)
   returns (bool) {
+    return _transfer(protocolId, msg.sender, to, tokenId);
+  }
+
+  function safeTransferFrom(
+        uint256 protocolId,
+        address from,
+        address to,
+        uint256 tokenId
+    ) external override {
+      this.safeTransferFrom(protocolId, from, to, tokenId, "");
+    }
+  
+  function safeTransferFrom(
+        uint256 protocolId,
+        address from,
+        address to,
+        uint256 tokenId,
+        bytes calldata data
+    )
+    external
+    override
+    protocolExists(protocolId)
+    validAddress(from)
+    validAddress(to) {
+      require(_isApprovedOrOwner(protocolId, msg.sender, tokenId), "safeTransferFrom: transfer caller is not owner nor approved");
+      _transfer(protocolId, from, to, tokenId);
+  }
+
+  function _isApprovedOrOwner(uint256 protocolId, address spender, uint256 tokenId) public returns (bool) {
     mapping(uint256 => address) storage protoOwners = owners[protocolId];
-    require(protoOwners[tokenId] == msg.sender, "transferMy: The sender is not the owner");
+    address owner = protoOwners[tokenId];
+    require(owner != address(0), "_isApprovedOrOwner: such token doesn't exist");
+    return (owner == spender || getApproved(protocolId, tokenId) || isApprovedForAll(owner, spender));
+  }
+
+  function transferFrom(
+        uint256 protocolId,
+        address from,
+        address to,
+        uint256 tokenId
+    )
+    external
+    override
+    protocolExists(protocolId)
+    validAddress(from)
+    validAddress(to) {
+      require(_isApprovedOrOwner(protocolId, msg.sender, tokenId), "safeTransferFrom: transfer caller is not owner nor approved");
+      _transfer(protocolId, from, to, tokenId);
+  }
+
+  function _transfer(uint256 protocolId, address from, address to, uint256 tokenId)
+  internal
+  returns (bool) {
+    mapping(uint256 => address) storage protoOwners = owners[protocolId];
+    require(protoOwners[tokenId] == from, "_transfer: The from address is not the owner");
+    mapping(address => uint256) storage protoBalances = balances[protocolId];
+    protoBalances[from] -= 1;
+    protoBalances[to] += 1;
     protoOwners[tokenId] = to;
     return true;
   }
-
-  modifier 
 
   function approve(uint256 protocolId, address to, uint256 tokenId)
   external
@@ -125,7 +192,7 @@ contract ERC721ERC20Protocols is IERC721ERC20Protocols {
   }
 
   function getApproved(uint256 protocolId, uint256 tokenId)
-  external
+  public
   view
   override
   protocolExists(protocolId)
@@ -133,14 +200,23 @@ contract ERC721ERC20Protocols is IERC721ERC20Protocols {
     return approved[protocolId][tokenId];
   }
 
-  function setApprovalForAll(uint256 protocolId, address operator, bool _approved)
+  function setApprovalForAll(address operator, bool _approved)
   external
-  override 
-  protocolExists(protocolId) 
-  validAddress(operator){
-    mapping(uint256 => address) storage protoApproved = approved[protocolId];
-    require(protoApproved[tokenId] == msg.sender, "approve: The sender is not the owner");
-    // ...
+  override
+  validAddress(operator) {
+    require(msg.sender != operator, "setApprovalForAll: msg.sender is operator");
+    operatorsApproved[msg.sender][operator] = _approved;
+  }
+
+  function isApprovedForAll(address owner, address operator)
+  public
+  view
+  override
+  validAddress(owner)
+  validAddress(operator)
+  returns (bool) {
+    require(owner != operator, "isApprovedForAll: owner is operator");
+    return operatorsApproved[owner][operator];
   }
 
 }
